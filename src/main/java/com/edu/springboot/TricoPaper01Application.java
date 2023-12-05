bd96288fe4230c70aa63d353c2581a8f5a3883c8
nakjasabal <nakjasabal@naver.com>
2023-12-05 12:05:57 +0900

    0

---
+++ src/main/java/com/edu/springboot/TricoPaper01Application.java
@@ -0,0 +1,13 @@
+package com.edu.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TricoPaper01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TricoPaper01Application.class, args);
+	}
+
+}
